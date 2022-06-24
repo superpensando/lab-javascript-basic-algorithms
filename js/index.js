9e424f173594066b0274768116fe668a51961516
laroca <laroca73@gmail.com>
2022-06-24 20:37:19 +0200

    Iteration 3

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 console.log("I'm ready!");
 
 // Iteration 1: Names and Input
-let hacker1 = "Víctor";
+let hacker1 = "Joe";
 console.log("The driver's name is " + hacker1);
-let hacker2 = "Laura";
+let hacker2 = "Scarlett";
 console.log("The navigator's name is " + hacker2);
 
 
@@ -21,4 +21,29 @@ if (firstPossibility) {
     console.log(`The driver has the longest name, it has ${hacker1Length} characters`);
 } else if (thirdPossibility) {
     console.log(`It seems that the navigator has the longest name, it has ${hacker2Length} characters`);
+}
+
+// Iteration 3: Loops
+let hacker1Loop="";
+for (let i=0; i < hacker1Length; i++ ){
+    hacker1Loop += hacker1[i].toUpperCase() + " ";
+}
+console.log(hacker1Loop); 
+
+let hacker2Loop="";
+for (let i=(hacker2Length - 1); i >= 0 ; i-- ){
+    hacker2Loop += hacker2[i] + " ";
+}
+console.log(hacker2Loop);
+
+switch (hacker1Loop.localeCompare(hacker2Loop)) {
+    case 0: 
+        console.log("What?! You both have the same name?"); 
+        break;
+    case -1:
+        console.log("The driver's name goes first");
+        break;
+    case 1:
+        console.log("Yo, the navigator goes first definitely");
+        break;
 }
