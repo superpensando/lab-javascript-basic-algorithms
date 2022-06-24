444c8f0bd882991f25dc65003d63c2b6bda87137
laroca <laroca73@gmail.com>
2022-06-24 19:44:57 +0200

    Add Iteration1

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 console.log("I'm ready!");
 
+// Iteration 1: Names and Input
+let hacker1 = "Víctor"
+console.log("The driver's name is " + hacker1);
+let hacker2 = "Laura"
+console.log("The navigator's name is " + hacker2);
+
+
 
