b3de474e95752ffd1f34367d732a3096d34d09e5
laroca <laroca73@gmail.com>
2022-06-24 21:27:21 +0200

    Add Bonus 1

--- js/index.js
+++ js/index.js
@@ -47,3 +47,30 @@ switch (hacker1Loop.localeCompare(hacker2Loop)) {
         console.log("Yo, the navigator goes first definitely");
         break;
 }
+
+// Bonus Time!
+// Bonus1
+const loremParagraph= `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sodales mollis nunc. Suspendisse et erat erat. Curabitur quis consectetur justo. Etiam elementum rhoncus posuere. Donec non egestas metus. Cras gravida est velit, vitae sagittis sem commodo vitae. Sed at leo ac dolor laoreet semper id et elit. Nullam condimentum at dolor et euismod. Nulla hendrerit vehicula sollicitudin. Maecenas dapibus ante eros, a vestibulum nisl sodales eu. Fusce nec nunc sit amet urna ornare sagittis id ut erat. 
+Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin eget molestie nunc, in blandit eros. Mauris commodo blandit dui et posuere. Mauris nisi libero, luctus nec suscipit aliquam, rhoncus vitae mauris. Phasellus convallis nec est quis bibendum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse dictum tellus id arcu aliquam cursus at in ante. 
+Integer in lectus dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dapibus in mi quis tristique. Vestibulum non molestie leo. Quisque ac nisl eu mauris dignissim finibus at condimentum elit. Mauris tempor ligula id imperdiet viverra. Etiam in lacus et mi consectetur pharetra. Suspendisse congue purus quis eros ultricies, ut pellentesque nibh interdum. Ut mi eros, vulputate at augue quis, suscipit euismod lorem. Suspendisse potenti.`;
+
+console.log(`The number of characters in loremParagraph is ${loremParagraph.length}`);
+
+const loremParagraphLength=loremParagraph.split(" ").length;
+console.log(`The number of words in loremParagraph is ${loremParagraphLength}`);
+
+//way 1
+const loremParagraphEtLength=loremParagraph.split(" et ").length - 1;
+console.log(`the number of times the Latin word " et " appears in loremParagraph is ${loremParagraphEtLength}`);
+
+//way 2
+let count = 0 ;
+for (let i = 0; i < loremParagraph.length; i++) { 
+    if ((loremParagraph[i]=== " ") && (loremParagraph[i+1]=== "e") && (loremParagraph[i+2]=== "t") && (loremParagraph[i+3]=== " ")) {
+          count = count + 1;       
+    }    
+}
+console.log(`the number of times the Latin word " et " appears in loremParagraph is ${count}`);
+  
+
+// Bonus2
